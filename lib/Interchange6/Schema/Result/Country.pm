2b83d89c8209e57dfb40e427e0d49406f5f0f2f3
Sam Batschelet <sbatschelet@mac.com>
2013-12-07 11:26:10 -0500

    Add Review and Country schemas

---
+++ lib/Interchange6/Schema/Result/Country.pm
@@ -0,0 +1,116 @@
+use utf8;
+package Interchange6::Schema::Result::Country;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Interchange6::Schema::Result::Country
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<country>
+
+=cut
+
+__PACKAGE__->table("country");
+
+=head1 DESCRIPTION
+
+ISO 3166-1 codes for country identification
+
+B<iso_code:> Two letter country code such as 'sl' = Slovenia.
+
+B<scope:> Internal sorting field.
+
+B<name:>  Full country name.
+
+B<priority:>  Display sorting.
+
+B<active:>  Is this country an active shipping destination?
+
+=cut
+
+=head1 ACCESSORS
+
+=head2 iso_code
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 2
+
+=head2 scope
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 32
+
+=head2 name
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 priority
+
+  data_type: 'integer'
+  default_value: 0
+  is_nullable: 0
+
+=head2 show_state
+
+  data_type: 'boolean'
+  default_value: true
+  is_nullable: 0
+
+=head2 active
+
+  data_type: 'boolean'
+  default_value: true
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "iso_code",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 2 },
+  "scope",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 32 },
+  "name",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "priority",
+  { data_type => "integer", default_value => 0, is_nullable => 0 },
+  "show_states",
+  { data_type => "boolean", default_value => \"true", is_nullable => 0 },
+  "active",
+  { data_type => "boolean", default_value => \"true", is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</iso_code>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("iso_code");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-12-06 07:40:36
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:4ycBkFKPUPXEOv3/IfNayw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
---
+++ lib/Interchange6/Schema/Result/Review.pm
@@ -0,0 +1,175 @@
+use utf8;
+package Interchange6::Schema::Result::Review;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Interchange6::Schema::Result::Review
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->load_components(qw(TimeStamp));
+
+=head1 TABLE: C<reviews>
+
+=cut
+
+__PACKAGE__->table("reviews");
+
+=head1 DESCRIPTION
+
+User product reviews
+
+B<reviews_id:>
+
+B<sku:>
+
+B<title:>
+
+B<content:>
+
+B<users_id:>  Id of user making review NULL if anonymous.
+
+B<name:>  Displayed vs actual name of user.
+
+B<rating:>  Numeric range 1.0 to 5.0 for item rating.
+
+B<public:>  Default is false
+
+B<approved:>  Default is false
+
+B<created:>
+
+=cut
+
+=head1 ACCESSORS
+
+=head2 reviews_id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 sku
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 32
+
+=head2 title
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 content
+
+  data_type: 'text'
+  is_nullable: 0
+
+=head2 users_id
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=head2 name
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 rating
+
+  data_type: 'numeric'
+  default_value: 0.0
+  is_nullable: 0
+
+=head2 public
+
+  data_type: 'boolean'
+  default_value: false
+  is_nullable: 0
+
+=head2 approved
+
+  data_type: 'boolean'
+  default_value: false
+  is_nullable: 0
+
+=head2 created
+
+  data_type: 'datetime'
+  set_on_create: 1
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "reviews_id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "sku",
+  { data_type => "varchar", is_nullable => 0, size => 32 },
+  "title",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "content",
+  { data_type => "text", is_nullable => 0 },
+  "users_id",
+  { data_type => "integer", is_nullable => 1 },
+  "name",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "rating",
+  { data_type => "numeric", default_value => 0.0, is_nullable => 0 },
+  "public",
+  { data_type => "boolean", default_value => \"false", is_nullable => 0 },
+  "approved",
+  { data_type => "boolean", default_value => \"false", is_nullable => 0 },
+  "created",
+  { data_type => "datetime", set_on_create => 1, is_nullable => 0 },
+
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</reviews_id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("reviews_id");
+
+=head1 RELATIONS
+
+=head2 user
+
+Type: belongs_to
+
+Related object: L<Interchange6::Schema::Result::User>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "user",
+  "Interchange6::Schema::Result::User",
+  { users_id => "users_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-12-05 08:49:16
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:JtaRif8NDVRTymg4pwVYMg
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
