b4e5c3b3089d5d5b69b59251abf286eba99ffbd7
Sam Batschelet <sbatschelet@mac.com>
2013-12-15 16:17:09 -0500

    Change return value of StateLocale to array reference and improve indentations

--- lib/Interchange6/Schema/Populate/StateLocale.pm
+++ lib/Interchange6/Schema/Populate/StateLocale.pm
@@ -17,23 +17,25 @@ use Moo;
 use Locale::SubCountry;
 
 sub records {
-    my $states;
-    my %countries = Interchange6::Schema::Populate::CountryLocale->records();
+    my @states;
+    my $countries = Interchange6::Schema::Populate::CountryLocale->new->records;
 
-    # populate states hash
-    foreach my $country ( sort keys %countries ) {
-    if (exists $countries{'show_states' => '1'}) {
-    my $country_code = new Locale::SubCountry($country);
-    my %country_states_keyed_by_code = $sub_country_code->code_full_name_hash;
+    for my $country_object (@$countries) {
+        if ($country_object->{'show_states'} == 1){
+        my $country_code = $country_object->{'country_iso_code'};
+        my $country = new Locale::SubCountry( $country_object->{'country_iso_code'} );
+        my %country_states_keyed_by_code = $country->code_full_name_hash;
 
-        foreach my $state_code ( sort keys %country_states_keyed_by_code ){
-        my $state_name = $country_states_keyed_by_code{$state_code};
-        # remode (Junk) from some records
-        $state_name =~ s/\s*\([^)]*\)//g;
-        $states{$state_name} = {'state_iso_code' => $state_code, 'country_iso_code' => $country_code};
+            foreach my $state_code ( sort keys %country_states_keyed_by_code ){
+                my $state_name = $country_states_keyed_by_code{$state_code};
+                # remode (Junk) from some records
+                $state_name =~ s/\s*\([^)]*\)//g;
+                push @states, {'name' => $state_name, 'state_iso_code' => $state_code, 'country_iso_code' => $country_code};
+            }
         }
     }
-    return %states;
+    
+    return \@states;
 }
 
 1;
