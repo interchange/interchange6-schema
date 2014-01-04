79c574fbe8d551a5fecb98ec7c06a0bfde8b0fd9
Sam Batschelet <sbatschelet@mac.com>
2014-01-04 16:06:11 -0500

    Add proper POD coverage

--- lib/Interchange6/Schema/ResultSet/Session.pm
+++ lib/Interchange6/Schema/ResultSet/Session.pm
@@ -7,6 +7,12 @@ Interchange6::Schema::ResultSet::Session
 
 =cut
 
+=head1 SYNOPSIS
+
+Check for expired sessions and carts.
+
+=cut
+
 use strict;
 use warnings;
 
@@ -14,6 +20,15 @@ use DateTime;
 use Time::Duration::Parse;
 
 use base 'DBIx::Class::ResultSet';
+
+=head2 expire( $expires )
+
+Checks for expired sessions where I<$expires> is either in seconds or
+human readable format.  Example '2 hours'.  If the cart user is
+registered the session will be deleted but and the cart's session_id
+will be made NULL.  Anonymous sessions/carts will be deleted.
+
+=cut
 
 sub expire {
     my ($self, $expires) = @_;
