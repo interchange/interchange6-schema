403f6eb72ae7a6ffcae8bee24d7af3308583e736
Stefan Hornburg (Racke) <racke@linuxia.de>
2014-02-23 10:42:27 +0100

    Bump up version number.

--- lib/Interchange6/Schema.pm
+++ lib/Interchange6/Schema.pm
@@ -7,7 +7,7 @@ Interchange6::Schema - Database Schema for Interchange 6
 
 =head1 VERSION
 
-0.014
+0.015
 
 =head1 DESCRIPTION
 
@@ -192,7 +192,7 @@ See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
-our $VERSION = '0.014';
+our $VERSION = '0.015';
 
 # Created by DBIx::Class::Schema::Loader
 # DO NOT MODIFY THE FIRST PART OF THIS FILE
