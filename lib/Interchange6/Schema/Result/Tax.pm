ef4223b0a1bb145c5ed5daf075667fa800a5acea
Peter Mottram <peter@sysnix.com>
2014-03-27 19:22:41 +0100

    Tax testing now much more complete. Should be ready for merge.

--- lib/Interchange6/Schema/Result/Tax.pm
+++ lib/Interchange6/Schema/Result/Tax.pm
@@ -11,8 +11,13 @@ Interchange6::Schema::Result::Tax
 use strict;
 use warnings;
 use DateTime;
+use POSIX qw/ceil floor/;
 
-use base 'DBIx::Class::Core';
+use Moo;
+
+extends 'DBIx::Class::Core';
+
+use namespace::clean;
 
 # component load order is important so be careful here:
 __PACKAGE__->load_components(
@@ -55,6 +60,23 @@ The taxes table contains taxes such as sales tax and VAT. Each tax has a unique 
   is_nullable: 0
   size: [3,4]
 
+=head2 precision
+
+  data_type: 'integer'
+  is_nullable: 0
+  default_value: 2
+
+Number of decimal places of precision required. Defaults to 2.
+
+=head2 rounding
+
+  data_type: char"
+  is_nullable: 1
+  size: 1
+  default_value: undef
+
+Default rounding is half round up to the precision number of decimal places. To use floor or ceiling set rounding to 'f' or 'c' as appropriate. The rounding value is automatically converted to lower case and any invalid value passed in will be ignored and instead undef will be stored giving default rounding.
+
 =head2 valid_from
 
   data_type: 'date'
@@ -66,17 +88,17 @@ The taxes table contains taxes such as sales tax and VAT. Each tax has a unique 
   data_type: 'date'
   is_nullable: 1
 
+=head2 country_iso_code
+
+  data_type: 'char'
+  is_foreign_key: 1
+  is_nullable: 1
+
 =head2 states_id
 
   data_type: 'integer'
   is_foreign_key: 1
   is_nullable: 1
-
-=head2 country_iso_code
-
-  data_type: 'char'
-  is_foreign_key: 1
-  is_nullable: 0
 
 =head2 created
 
@@ -107,14 +129,23 @@ __PACKAGE__->add_columns(
     { data_type => "varchar", is_nullable => 0, size => 64 },
     "percent",
     { data_type => "numeric", is_nullable => 0, size => [ 3, 4 ] },
+    "precision",
+    { data_type => "integer", is_nullable => 0, default_value => 2 },
+    "rounding",
+    {
+        data_type     => "char",
+        is_nullable   => 1,
+        size          => 1,
+        default_value => undef
+    },
     "valid_from",
     { data_type => "date", set_on_create => 1, is_nullable => 0 },
     "valid_to",
     { data_type => "date", is_nullable => 1 },
+    "country_iso_code",
+    { data_type => "char", is_foreign_key => 1, is_nullable => 1 },
     "states_id",
     { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
-    "country_iso_code",
-    { data_type => "char", is_foreign_key => 1, is_nullable => 0 },
     "created",
     { data_type => "datetime", set_on_create => 1, is_nullable => 0 },
     "last_modified",
@@ -126,19 +157,24 @@ __PACKAGE__->add_columns(
     },
 );
 
+# for rounding to only store undef, c or f
+
+around rounding => sub {
+    my ($orig, $self, @a) = (shift, shift, @_);
+
+    if ( $a[0] ) {
+        if ( $a[0] =~ m/^(f|c)/i ) {
+            $a[0] = lc($1);
+        }
+        else {
+            $a[0] = undef;
+        }
+    }
+
+    $self->$orig(@a);
+};
+
 =head1 METHODS
-
-=head2 sqlt_deploy_hook
-
-Called during table creation to add indexes on the following columns:
-
-=cut
-
-sub sqlt_deploy_hook {
-    my ( $self, $sqlt_table ) = @_;
-
-    $sqlt_table->add_index( name => 'idx_name', fields => ['name'] );
-}
 
 =head2 calculate
 
@@ -154,18 +190,13 @@ Price of product either inclusive or exclusive of tax - required.
 
 =item * tax_included
 
-Boolean indicating whether price is inclusive of tax or not. Defaults to 0.
-
-=item * precision
-
-How many decimal places to round the result to. Defaults to 2.
+Boolean indicating whether price is inclusive of tax or not. Defaults to 0 which means exclusive of tax.
 
 =back
 
 Usage example:
 
-    my $tax = $taxrecord->caclulate({ price => 13.47, 
-        tax_included => 1, precision => 2 });
+    my $tax = $taxrecord->caclulate({ price => 13.47, tax_included => 1 });
 
     # with percentage 18 our tax is 2.05
 
@@ -192,9 +223,29 @@ sub calculate {
 
     # round & return
 
-    $args->{precision} = 2 unless $args->{precision};
+    my $precision = $self->precision;
 
-    return sprintf( "%.$args->{precision}f", $tax );
+    unless ( $self->rounding ) {
+
+        return sprintf( "%.${precision}f", $tax );
+    }
+    else {
+
+        $tax *= 10**$precision;
+
+        if ( $self->rounding eq 'c' ) {
+            $tax = ceil($tax) / ( 10**$precision );
+        }
+        elsif ( $self->rounding eq 'f' ) {
+            $tax = floor($tax) / ( 10**$precision );
+        }
+        else {
+            # should not be possible to get here - return undef
+            return undef;
+        }
+
+        return sprintf( "%.${precision}f", $tax );
+    }
 }
 
 =head1 PRIMARY KEY
@@ -227,7 +278,8 @@ __PACKAGE__->belongs_to(
         is_deferrable => 1,
         on_delete     => "CASCADE",
         on_update     => "CASCADE",
-        order_by      => 'name'
+        order_by      => 'name',
+        join_type     => 'left',
     }
 );
 
@@ -247,11 +299,58 @@ __PACKAGE__->belongs_to(
         is_deferrable => 1,
         on_delete     => "CASCADE",
         on_update     => "CASCADE",
-        order_by      => 'name'
+        order_by      => 'name',
+        join_type     => 'left',
     }
 );
 
 =head1 PRIVATE METHODS
+
+=head2 new
+
+We override the new method to set default values on certain rows at create time.
+
+=cut
+
+sub new {
+    my ( $class, $attrs ) = @_;
+
+    $attrs->{precision} = 2 unless defined $attrs->{precision};
+
+    my $new = $class->next::method($attrs);
+
+    return $new;
+}
+
+=head2 sqlt_deploy_hook
+
+Called during table creation to add indexes on the following columns:
+
+=over 4
+
+=item * tax_name
+
+=item * valid_from
+
+=item * valid_to
+
+=back
+
+=cut
+
+sub sqlt_deploy_hook {
+    my ( $self, $table ) = @_;
+
+    $table->add_index( name => 'taxes_idx_tax_name', fields => ['tax_name'] );
+    $table->add_index(
+        name   => 'taxes_idx_valid_from',
+        fields => ['valid_from']
+    );
+    $table->add_index(
+        name   => 'taxes_idx_valid_to',
+        fields => ['valid_to']
+    );
+}
 
 =head2 _validate
 
@@ -327,7 +426,7 @@ sub _validate {
         );
         if ( $rset->count > 0 ) {
             $self->add_result_error( 'error',
-                'tax overlaps existing date range' );
+                'tax overlaps existing date range: ' . $self->tax_name );
             return;
         }
     }
