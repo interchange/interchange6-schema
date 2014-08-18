1ffcae3e93cf0182c5242e81429fbafbb8b5c3e0
Peter Mottram <peter@sysnix.com>
2014-08-18 17:33:10 +0200

    move Address to ::Candy

--- lib/Interchange6/Schema/Result/Address.pm
+++ lib/Interchange6/Schema/Result/Address.pm
@@ -1,4 +1,5 @@
 use utf8;
+
 package Interchange6::Schema::Result::Address;
 
 =head1 NAME
@@ -10,15 +11,8 @@ Interchange6::Schema::Result::Address
 use strict;
 use warnings;
 
-use base 'DBIx::Class::Core';
-
-__PACKAGE__->load_components(qw(InflateColumn::DateTime TimeStamp));
-
-=head1 TABLE: C<addresses>
-
-=cut
-
-__PACKAGE__->table("addresses");
+use Interchange6::Schema::Candy -components =>
+  [qw(InflateColumn::DateTime TimeStamp)];
 
 =head1 ACCESSORS
 
@@ -28,12 +22,30 @@ __PACKAGE__->table("addresses");
   is_auto_increment: 1
   is_nullable: 0
   sequence: 'addresses_addresses_id_seq'
+  primary key
+
+=cut
+
+primary_column addresses_id => {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "addresses_addresses_id_seq",
+};
 
 =head2 users_id
 
   data_type: 'integer'
   is_foreign_key: 1
   is_nullable: 0
+
+=cut
+
+column users_id => {
+    data_type      => "integer",
+    is_foreign_key => 1,
+    is_nullable    => 0,
+};
 
 =head2 type
 
@@ -42,11 +54,28 @@ __PACKAGE__->table("addresses");
   is_nullable: 0
   size: 16
 
+=cut
+
+column type => {
+    data_type     => "varchar",
+    default_value => "",
+    is_nullable   => 0,
+    size          => 16,
+};
+
 =head2 archived
 
   data_type: 'boolean'
   default_value: 0
   is_nullable: 0
+
+=cut
+
+column archived => {
+    data_type     => "boolean",
+    default_value => 0,
+    is_nullable   => 0,
+};
 
 =head2 first_name
 
@@ -55,6 +84,15 @@ __PACKAGE__->table("addresses");
   is_nullable: 0
   size: 255
 
+=cut
+
+column first_name => {
+    data_type     => "varchar",
+    default_value => "",
+    is_nullable   => 0,
+    size          => 255,
+};
+
 =head2 last_name
 
   data_type: 'varchar'
@@ -62,12 +100,31 @@ __PACKAGE__->table("addresses");
   is_nullable: 0
   size: 255
 
+=cut
+
+column last_name => {
+    data_type     => "varchar",
+    default_value => "",
+    is_nullable   => 0,
+    size          => 255,
+};
+
 =head2 company
+
 
   data_type: 'varchar'
   default_value: (empty string)
   is_nullable: 0
   size: 255
+
+=cut
+
+column company => {
+    data_type     => "varchar",
+    default_value => "",
+    is_nullable   => 0,
+    size          => 255,
+};
 
 =head2 address
 
@@ -76,12 +133,30 @@ __PACKAGE__->table("addresses");
   is_nullable: 0
   size: 255
 
+=cut
+
+column address => {
+    data_type     => "varchar",
+    default_value => "",
+    is_nullable   => 0,
+    size          => 255,
+};
+
 =head2 address_2
 
   data_type: 'varchar'
   default_value: (empty string)
   is_nullable: 0
   size: 255
+
+=cut
+
+column address_2 => {
+    data_type     => "varchar",
+    default_value => "",
+    is_nullable   => 0,
+    size          => 255,
+};
 
 =head2 postal_code
 
@@ -90,12 +165,30 @@ __PACKAGE__->table("addresses");
   is_nullable: 0
   size: 255
 
+=cut
+
+column postal_code => {
+    data_type     => "varchar",
+    default_value => "",
+    is_nullable   => 0,
+    size          => 255,
+};
+
 =head2 city
 
   data_type: 'varchar'
   default_value: (empty string)
   is_nullable: 0
   size: 255
+
+=cut
+
+column city => {
+    data_type     => "varchar",
+    default_value => "",
+    is_nullable   => 0,
+    size          => 255,
+};
 
 =head2 phone
 
@@ -104,11 +197,28 @@ __PACKAGE__->table("addresses");
   is_nullable: 0
   size: 32
 
+=cut
+
+column phone => {
+    data_type     => "varchar",
+    default_value => "",
+    is_nullable   => 0,
+    size          => 32,
+};
+
 =head2 states_id
 
   data_type: 'integer'
   is_foreign_key: 1
   is_nullable: 1
+
+=cut
+
+column states_id => {
+    data_type      => "integer",
+    is_foreign_key => 1,
+    is_nullable    => 1,
+};
 
 =head2 country_iso_code
 
@@ -117,11 +227,28 @@ __PACKAGE__->table("addresses");
   is_nullable: 0
   size: 2
 
+=cut
+
+column country_iso_code => {
+    data_type      => "char",
+    is_foreign_key => 1,
+    is_nullable    => 0,
+    size           => 2,
+};
+
 =head2 created
 
   data_type: 'datetime'
   set_on_create: 1
   is_nullable: 0
+
+=cut
+
+column created => {
+    data_type     => "datetime",
+    set_on_create => 1,
+    is_nullable   => 0,
+};
 
 =head2 last_modified
 
@@ -132,57 +259,12 @@ __PACKAGE__->table("addresses");
 
 =cut
 
-__PACKAGE__->add_columns(
-  "addresses_id",
-  {
-    data_type         => "integer",
-    is_auto_increment => 1,
-    is_nullable       => 0,
-    sequence          => "addresses_addresses_id_seq",
-  },
-  "users_id",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
-  "type",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 16 },
-  "archived",
-  { data_type => "boolean", default_value => 0, is_nullable => 0 },
-  "first_name",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
-  "last_name",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
-  "company",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
-  "address",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
-  "address_2",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
-  "postal_code",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
-  "city",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
-  "phone",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 32 },
-  "states_id",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
-  "country_iso_code",
-  { data_type => "char", is_foreign_key => 1, is_nullable => 0, size => 2 },
-  "created",
-  { data_type => "datetime", set_on_create => 1, is_nullable => 0 },
-  "last_modified",
-  { data_type => "datetime", set_on_create => 1, set_on_update => 1, is_nullable => 0 },
-);
-
-=head1 PRIMARY KEY
-
-=over 4
-
-=item * L</addresses_id>
-
-=back
-
-=cut
-
-__PACKAGE__->set_primary_key("addresses_id");
+column last_modified => {
+    data_type     => "datetime",
+    set_on_create => 1,
+    set_on_update => 1,
+    is_nullable   => 0,
+};
 
 =head1 RELATIONS
 
@@ -194,12 +276,10 @@ Related object: L<Interchange6::Schema::Result::OrderlinesShipping>
 
 =cut
 
-__PACKAGE__->has_many(
-  "orderlines_shipping",
-  "Interchange6::Schema::Result::OrderlinesShipping",
+has_many
+  orderlines_shipping => "Interchange6::Schema::Result::OrderlinesShipping",
   { "foreign.addresses_id" => "self.addresses_id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
+  { cascade_copy           => 0, cascade_delete => 0 };
 
 =head2 orders
 
@@ -209,12 +289,10 @@ Related object: L<Interchange6::Schema::Result::Order>
 
 =cut
 
-__PACKAGE__->has_many(
-  "orders",
-  "Interchange6::Schema::Result::Order",
+has_many
+  orders => "Interchange6::Schema::Result::Order",
   { "foreign.billing_addresses_id" => "self.addresses_id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
+  { cascade_copy                   => 0, cascade_delete => 0 };
 
 =head2 user
 
@@ -224,12 +302,10 @@ Related object: L<Interchange6::Schema::Result::User>
 
 =cut
 
-__PACKAGE__->belongs_to(
-  "user",
-  "Interchange6::Schema::Result::User",
-  { users_id => "users_id" },
-  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
-);
+belongs_to
+  user => "Interchange6::Schema::Result::User",
+  { users_id      => "users_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" };
 
 =head2 state
 
@@ -239,12 +315,10 @@ Related object: L<Interchange6::Schema::Result::State>
 
 =cut
 
-__PACKAGE__->belongs_to(
-  "state",
-  "Interchange6::Schema::Result::State",
-  { states_id => "states_id" },
-  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
-);
+belongs_to
+  state => "Interchange6::Schema::Result::State",
+  { states_id     => "states_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" };
 
 =head2 country
 
@@ -254,12 +328,10 @@ Related object: L<Interchange6::Schema::Result::Country>
 
 =cut
 
-__PACKAGE__->belongs_to(
-  "country",
-  "Interchange6::Schema::Result::Country",
-  { country_iso_code => "country_iso_code" },
-  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
-);
+belongs_to
+  country => "Interchange6::Schema::Result::Country",
+  "country_iso_code",
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" };
 
 =head2 orderlines
 
@@ -269,6 +341,6 @@ Composing rels: L</orderlines_shipping> -> orderline
 
 =cut
 
-__PACKAGE__->many_to_many("orderlines", "orderlines_shipping", "orderline");
+many_to_many orderlines => "orderlines_shipping", "orderline";
 
 1;
