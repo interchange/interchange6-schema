b7aeb3d1f04eab7c78853242c07b4c2ce5efc6d7
Stefan Hornburg (Racke) <racke@linuxia.de>
2014-11-27 16:30:37 +0100

    Bump up version number.

--- lib/Interchange6/Schema.pm
+++ lib/Interchange6/Schema.pm
@@ -10,11 +10,11 @@ Interchange6::Schema - Database Schema for Interchange 6
 
 =head1 VERSION
 
-0.060_03
+0.060
 
 =cut
 
-our $VERSION = '0.060_03';
+our $VERSION = '0.060';
 
 =head1 DESCRIPTION
 
