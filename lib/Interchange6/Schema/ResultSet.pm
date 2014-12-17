60349eb776ec92cd65cd4b0a922dec32302c3c22
Peter Mottram <peter@sysnix.com>
2014-12-17 12:44:14 +0100

    add Schema::ResultSet.pm to git

---
+++ lib/Interchange6/Schema/ResultSet.pm
@@ -0,0 +1,33 @@
+package Interchange6::Schema::ResultSet;
+
+=head1 NAME
+
+Interchange6::Schema::ResultSet - the default resultset class for Interchange6
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::ResultSet';
+
+=head1 DESCRIPTION
+
+This is the default resultset class from which all custom ResultSet classes
+should inherit.
+
+It is defined as the default resultset class in L<Interchange6::Schema>.
+We use this class to load components that we want to make available to all
+of our resultsets. Current the following components are loaded:
+
+=over
+
+=item * L<DBIx::Class::Helper::ResultSet::Shortcut>
+
+=back
+
+=cut
+ 
+__PACKAGE__->load_components('Helper::ResultSet::Shortcut');
+ 
+1;
