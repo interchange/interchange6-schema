7009debc222b684e1ddeac237fbab911f35a66f1
Peter Mottram <peter@sysnix.com>
2015-03-04 15:29:07 +0100

    add created and last_modified columns to ShipmentMethod

--- lib/Interchange6/Schema/Result/ShipmentMethod.pm
+++ lib/Interchange6/Schema/Result/ShipmentMethod.pm
@@ -8,7 +8,8 @@ Interchange6::Schema::Result::ShipmentMethod
 
 =cut
 
-use Interchange6::Schema::Candy;
+use Interchange6::Schema::Candy -components =>
+  [qw(InflateColumn::DateTime TimeStamp)];
 
 =head1 ACCESSORS
 
@@ -109,6 +110,31 @@ column shipment_carriers_id =>
 
 column active =>
   { data_type => "boolean", default_value => 1, is_nullable => 0 };
+
+=head2 created
+
+Date and time when this record was created returned as L<DateTime> object.
+Value is auto-set on insert.
+
+=cut
+
+column created => {
+    data_type     => "datetime",
+    set_on_create => 1
+};
+
+=head2 last_modified
+
+Date and time when this record was last modified returned as L<DateTime> object.
+Value is auto-set on insert and update.
+
+=cut
+
+column last_modified => {
+    data_type     => "datetime",
+    set_on_create => 1,
+    set_on_update => 1
+};
 
 =head1 RELATIONS
 
