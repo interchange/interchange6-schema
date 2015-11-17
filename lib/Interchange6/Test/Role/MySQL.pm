d0b3d28b2a594cef60a8af633a2b860a784d038f
Peter Mottram (SysPete) <peter@sysnix.com>
2015-11-17 20:34:29 +0100

    update connect options to current best practice

--- lib/Interchange6/Test/Role/MySQL.pm
+++ lib/Interchange6/Test/Role/MySQL.pm
@@ -80,7 +80,6 @@ sub connect_info {
             mysql_enable_utf8 => 1,
             on_connect_call => 'set_strict_mode',
             quote_names => 1,
-            PrintError => 1,
         }
     );
 }
