2a81e7a2e2268cb8a3c770f4178ce15f091f331f
Peter Mottram <peter@sysnix.com>
2015-12-08 18:40:41 +0100

    first 'working' version of InflateColumn::Currency

--- lib/Interchange6/Schema/InflateColumn/Currency.pm
+++ lib/Interchange6/Schema/InflateColumn/Currency.pm
@@ -8,63 +8,84 @@ Interchange6::Schema::InflateColumn::Currency
 
 use strict;
 use warnings;
-
-use base qw/DBIx::Class Class::Accessor::Grouped/;
+use base qw/DBIx::Class/;
 use Interchange6::Currency;
 use namespace::clean;
 
-__PACKAGE__->mk_group_accessors(
-    'simple', qw/
-      currency_code currency_format currency_code_column
-      /
-);
+__PACKAGE__->load_components(qw/Helper::Row::OnColumnChange/);
+
+=head1 SYNOPSIS
+
+ __PACKAGE__->load_components('+Interchange6::Schema::InflateColumn::Currency');
+ __PACKAGE__->add_columns(
+   price => { data_type => 'numeric', size => [13,3], is_currency => 1 },
+   currency_iso_code => { data_type => 'char', size => 3 },
+ );
 
 =head1 METHODS
 
 =head2 register_column
 
+Override L<DBIx::Class::Row/register_column> to add C<inflate_column> method
+to any columns defined with C<< is_currency => 1 >>. Column is inflated
+to L<Interchange6::Schema> object.  This would not normally be directly
+called by end users.
+
+=head1 COLUMN_INFO
+
+=head2 is_currency
+
+Must be set to true to cause inflation of the column.
+
+=head2 currency_code_column
+
+Name of the column that contains the 3 letter ISO currency code for this
+column. Defaults to C<currency_iso_code>.
+
 =cut
 
 sub register_column {
     my ( $self, $column, $info, @rest ) = @_;
+
     $self->next::method( $column, $info, @rest );
 
-    return unless defined $info->{'is_currency'};
-
-    my $currency_class =
-         $info->{'currency_class'}
-      || $self->currency_class
-      || 'Interchange6::Currency';
-
-    my $currency_iso_code =
-      $info->{'currency_iso_code'} || $self->currency_iso_code;
+    return unless $info->{'is_currency'};
 
     my $currency_code_column =
-      $info->{'currency_code_column'} || $self->currency_code_column;
-
-    my $currency_format = $info->{'currency_format'} || $self->currency_format;
+      $info->{'currency_code_column'} || 'currency_iso_code';
 
     $self->inflate_column(
         $column => {
             inflate => sub {
                 my ( $value, $obj ) = @_;
-                my $locale;
-                my $code =
-                    $currency_code_column
-                  ? $obj->$currency_code_column || $currency_iso_code
-                  : $currency_iso_code;
+                my $code;
+
+                if ( $obj->result_source->has_column($currency_code_column) ) {
+                    my $code = $obj->$currency_code_column;
+                }
+                if ( !$code ) {
+                    $code = $obj->result_source->schema->currency_iso_code;
+                }
 
                 return Interchange6::Currency->new(
                     value         => $value,
                     currency_code => $code,
-                    locale        => $locale
+                    locale        => $obj->result_source->schema->locale,
                 );
             },
             deflate => sub {
-                return shift->value;
+                my ( $value, $obj ) = @_;
+                return 0 + $value;
             },
         }
     );
+
+    # FIXME: this SUCKS big time but so far I haven't found a way around this.
+    # To see why this is needed just comment out the following line then run
+    # all tests and you will see that $product->update({price => $newprice})
+    # seems to work fine but then on checking the price you still see the old
+    # one.
+    $self->after_column_change( price => { method => 'discard_changes' } );
 }
 
 1;
