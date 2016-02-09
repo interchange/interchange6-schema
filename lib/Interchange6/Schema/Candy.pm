53460d3da3c9c65dfeb0e07e1a0f6d5bf7d38384
Peter Mottram <peter@sysnix.com>
2016-02-09 15:15:59 +0100

    add Devel::Cover uncoverable comments to Candy class

--- lib/Interchange6/Schema/Candy.pm
+++ lib/Interchange6/Schema/Candy.pm
@@ -17,6 +17,8 @@ Set base to either what is set by the Result class or else to DBIx::Class::Core
 
 =cut
 
+# we want to know if a class tries to use a difference base class
+# uncoverable branch false
 sub base { $_[1] || 'DBIx::Class::Core' }
 
 =head2 autotable
@@ -25,6 +27,8 @@ Set autotable to either what is set by the Result class or else to 1
 
 =cut
 
+# we want to know if a class tries to set its table name
+# uncoverable branch false
 sub autotable { $_[1] || 1 }
 
 1;
