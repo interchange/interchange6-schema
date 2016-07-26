7086117fbf155de6ea5ad7c44f4b9692628a32df
Peter Mottram <peter@sysnix.com>
2016-07-26 11:19:19 +0200

    add Component::DynamicDefaults to set website_id on create
    If Schema's current_website is set then we use that to set the value of
    website column when creating rows in all result classes (except
    Website).

--- lib/Interchange6/Schema/Candy.pm
+++ lib/Interchange6/Schema/Candy.pm
@@ -31,4 +31,29 @@ Set autotable to either what is set by the Result class or else to 1
 # uncoverable branch false
 sub autotable { $_[1] || 1 }
 
+=head2 parse_arguments
+
+Override L<DBIx::Class::Candy/parse_arguments> to add components to all of our
+Result classes. The following components are currently loaded:
+
+=over
+
+=item * Interchange6::Schema::Component::DynamicDefaults
+
+=back
+
+=cut
+
+sub parse_arguments {
+    my $self = shift;
+
+    # add components to this list:
+    my @result_class_components =
+      ('+Interchange6::Schema::Component::DynamicDefaults');
+
+    my $args = $self->next::method(@_);
+    unshift @{$args->{components}}, @result_class_components;
+    return $args;
+}
+
 1;
